25643341914bed0b3decfa221353d7d36d3cc27e
Avinash Agrawal <avinash03082003@gmail.com>
2024-01-02 13:06:58 +0530

    Added 02 JAN POTD

---
+++ 02_JAN_Largest_Increasing_Subsequence_Solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int Solution::lis(const vector<int> &A) {
+    int n = A.size();
+    int sol = 0;
+    vector<int> v(n);
+    for(int i = n-1; i>=0; i--){
+        int ans = 0;
+        for(int j = i+1; j<n; j++){
+            if(A[i] < A[j]){
+                ans = max(ans,v[j]);
+            }
+        }
+        v[i] = ans + 1;
+        sol = max(sol,v[i]);
+    }
+    return sol;
+}
