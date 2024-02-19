e1e49312039f30761ce98880a3ff645faf75d872
Avinash Agrawal <avinash03082003@gmail.com>
2024-02-19 19:25:59 +0530

    Added Feb 19 POTD

--- 19_Feb_Remove_Duplicates_From_Sorted_List_II_Solution.cpp
+++ 19_Feb_Remove_Duplicates_From_Sorted_List_II_Solution.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-ListNode deleteDuplicates(ListNode *A)
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* deleteDuplicates(ListNode *A)
 {
     if (!A || !(A->next))
         return A;
