13e8a943f6073da761016d8d1c26036a5a46bce6
Avinash Agrawal <avinash03082003@gmail.com>
2024-03-15 23:45:25 +0530

    Added 15 March POTD

---
+++ 15_March_Next_Permutation_Solution.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> nextPermutation(vector<int> &A) {
+    next_permutation(A.begin(),A.end());
+    return A;
+}
+
+
+int main(){
+    return 0;
+}
