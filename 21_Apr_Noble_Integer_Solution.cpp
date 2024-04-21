5df0cb7f950050d6fde48c7af708cd9ebac82a84
Avinash Agrawal <avinash03082003@gmail.com>
2024-04-21 13:17:50 +0530

    Added 21 Apr POTD

---
+++ 21_Apr_Noble_Integer_Solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int solve(vector<int> &A)
+{
+    int n=A.size(),cnt=n;
+   
+    map<int,int> mp;
+    for(int i=0;i<n;i++) mp[A[i]]++;
+   
+    for(auto it:mp)
+    {
+        cnt = cnt-it.second;
+        if(cnt==it.first) return 1;
+    }
+    return -1;
+}
+int main()
+{
+    return 0;
+}
