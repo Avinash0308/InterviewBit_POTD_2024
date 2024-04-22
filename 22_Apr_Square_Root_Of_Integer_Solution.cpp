97c3ea89a06ce692583f05308edbc4f3fdeb84ff
Avinash Agrawal <avinash03082003@gmail.com>
2024-04-22 20:46:38 +0530

    Added 22 Apr POTD

---
+++ 22_Apr_Square_Root_Of_Integer_Solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sqrt(int A)
+{
+    long long int a = 0;
+    while ((a + 1) * (a + 1) <= A)
+        a++;
+    return a;
+}
+
+int main()
+{
+    return 0;
+}
